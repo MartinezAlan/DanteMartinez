a9860ac6c84ac73e64f37705526afcbb45b96634
Dante Martinez <114031071+MartinezAlan@users.noreply.github.com>
2023-05-11 20:31:47 -0300

    Add files via upload

---
+++ Venta/Venta/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Venta
+{
+    private int id;
+    private string comentarios;
+    private long idUsuario;
+
+    public int Id
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string Comentarios
+    {
+        get { return comentarios; }
+        set { comentarios = value; }
+    }
+
+    public long IdUsuario
+    {
+        get { return idUsuario; }
+        set { idUsuario = value; }
+    }
+
+    public Venta()
+    {
+        id = 0;
+        comentarios = string.Empty;
+        idUsuario = 0;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Venta venta = new Venta();
+        venta.Id = 001;
+        venta.Comentarios = "Venta realizada con éxito";
+        venta.IdUsuario = 1001;
+
+        Console.WriteLine("Información de la Venta:");
+        Console.WriteLine("ID: " + venta.Id);
+        Console.WriteLine("Comentarios: " + venta.Comentarios);
+        Console.WriteLine("ID de Usuario: " + venta.IdUsuario);
+
+        Console.ReadLine();
+    }
+}
