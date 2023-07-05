903bcb6416ac22138563c93245f1dfbd0d740aa0
Dante Martinez <114031071+MartinezAlan@users.noreply.github.com>
2023-07-04 23:42:05 -0300

    Add files via upload

---
+++ Proyecto Final Martinez Dante/Controllers/NombreLocalController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Controllers.DTOS;
+
+namespace ProyectoFinal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class NombreLocalController : ControllerBase
+    {
+        
+
+
+        [HttpGet(Name = "TraerNombreDelLocal")] 
+        public string TraerNombreDelLocal()
+        {
+            return "Tienda El Pollo";
+        }
+    }
+}
---
+++ Proyecto Final Martinez Dante/Controllers/ProductoVendidoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoFinal.Controllers.DTOS;
+
+namespace ProyectoFinal.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ProductoVendidoController : ControllerBase
+    {
+       
+
+
+        [HttpGet(Name = "TraerProductosVendidos")]
+        public List<ProductoVendido> TraerProductosVendidos_conIdUsuario(long idUsuario)
+        {
+            return ProductoVendidoHandler.TraerProductosVendidos_conIdUsuario(idUsuario);
+        }
+    }
+}
